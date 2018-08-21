586d748d63bf2882a72c2f4c34d0e40c70389c85
David <david-zimmermann@hotmail.de>
2018-08-21 09:45:04 +0200

    added proper function addressing
    as we now don't export the "helper" functions, we need to prepend these function-calls with tidyAnimatedVerbs:::

--- R/animate_full_join.R
+++ R/animate_full_join.R
@@ -1,10 +1,10 @@
-#' Title
+#' Animates a full join between two datasets
 #'
-#' @param x
-#' @param y
+#' @param x the left dataset to join
+#' @param y the right dataset to join
 #' @param result gif or static, if static, only the last frame is shown.
 #'
-#' @return
+#' @return a gif or a ggplot image
 #' @export
 #'
 #' @examples
@@ -27,45 +27,45 @@
 #'
 #'  # to save the gif you can use
 #'  fj <- animate_full_join(x, y)
-#'  anim_save(here::here("images", "full-join.gif"), fj)
+#'  anim_save("full-join.gif", fj)
 #' }
 animate_full_join <- function(x, y, result = "gif") {
 
   check_xy_format(x)
   check_xy_format(y)
 
-  initial_join_dfs <- proc_data(x, "x") %>%
-    bind_rows(mutate(proc_data(y, "y"), .x = .x + 3)) %>%
+  initial_join_dfs <- tidyAnimatedVerbs:::proc_data(x, "x") %>%
+    bind_rows(mutate(tidyAnimatedVerbs:::proc_data(y, "y"), .x = .x + 3)) %>%
     mutate(frame = 1)
 
   if (result == "gif") {
     fj_joined_df <- full_join(x, y, "id") %>%
-      proc_data("x") %>%
+      tidyAnimatedVerbs:::proc_data("x") %>%
       mutate(.id = ifelse(value %in% c("4", "y4"), "y", .id)) %>%
       mutate(frame = 2, .x = .x + 1)
 
     fj_extra_blocks <- inner_join(x, y, "id") %>%
       select(id) %>%
-      proc_data("y") %>%
+      tidyAnimatedVerbs:::proc_data("y") %>%
       mutate(frame = 2, .x = .x + 1)
 
     fj <- initial_join_dfs %>%
       bind_rows(fj_joined_df, fj_extra_blocks) %>%
-      plot_data("full_join(x, y)") +
+      tidyAnimatedVerbs:::plot_data("full_join(x, y)") +
       transition_states(frame, transition_length = 2, state_length = 1) +
       enter_appear() +
       exit_disappear(early = TRUE) +
       ease_aes("sine-in-out")
 
     res <- animate(fj)
-    # anim_save(here::here("images", "full-join.gif"), fj)
 
   } else if (result == "static") {
 
     res <- full_join(x, y, "id") %>%
-      proc_data() %>%
+      tidyAnimatedVerbs:::proc_data() %>%
       mutate(.x = .x + 1) %>%
-      plot_data_join("full_join(x, y)", ylims = ylim(-4.5, -0.5))
+      tidyAnimatedVerbs:::plot_data_join("full_join(x, y)",
+                                         ylims = ylim(-4.5, -0.5))
   }
 
   return(res)
