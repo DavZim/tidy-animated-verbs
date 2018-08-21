5cb5d63a526c13762a985db52e029d4bf4dcb88b
David <david-zimmermann@hotmail.de>
2018-08-21 13:07:19 +0200

    set default values for the static file export

--- R/save_static_plot.R
+++ R/save_static_plot.R
@@ -1,17 +1,22 @@
-#' Saves a static plot
+#' Saves a static plot into different folders (see \code{\link{static_plot_filename}})
 #'
-#' @param g
-#' @param filename
-#' @param formats
+#' @param g a ggplot object as returned by \code{\link{plot_data}}
+#' @param filename the filename
+#' @param formats the format for the files
+#' @param width the width of the plot
+#' @param height the height of the plot
 #'
-#' @return
+#' @return NULL
+#' @export
 #'
 #' @examples
-save_static_plot <- function(g, filename, formats = c("png", "svg")) {
+#' NULL
+save_static_plot <- function(g, filename, formats = c("png", "svg"), width = 7,
+                             height = 7) {
   filenames <- formats %>%
     purrr::set_names() %>%
-    purrr::map_chr(static_plot_filename, x = filename) %>%
+    purrr::map_chr(tidyAnimatedVerbs:::static_plot_filename, x = filename) %>%
     purrr::iwalk(
-      ~ ggsave(filename = .x, plot = g, dev = .y)
+      ~ ggsave(filename = .x, plot = g, dev = .y, height = height, width = width)
     )
 }
