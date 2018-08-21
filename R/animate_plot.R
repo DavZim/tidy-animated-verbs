62969772b6625dbd66ae08b4546cdcb9c26d88e8
David <david-zimmermann@hotmail.de>
2018-08-21 09:43:25 +0200

    fixed translation error in animate_plot
    missing arguments in function definition added.

--- R/animate_plot.R
+++ R/animate_plot.R
@@ -1,11 +1,13 @@
 #' Animates a Plot
 #'
 #' @param x
+#' @param transition_length
+#' @param state_length
 #'
 #' @return
 #'
 #' @examples
-animate_plot <- function(x) {
+animate_plot <- function(x, transition_length = 2, state_length = 1) {
   x +
     transition_states(frame, transition_length, state_length) +
     enter_fade() +
